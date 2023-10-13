2fdfad6587c7672ff7c33437c989f73d232d6ad1
HOYA <als77770@naver.com>
2023-10-13 13:15:55 +0900

    더 정교한 달력 찍기, 진짜 달력 자동으로 찍기

--- src/main/java/calender/Calender.java
+++ src/main/java/calender/Calender.java
@@ -1,9 +1,23 @@
 package calender;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Calender {
 
     private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final String[] DAYS_OF_WEEK = {
+        DayOfWeek.SUNDAY.name(),
+        DayOfWeek.MONDAY.name(),
+        DayOfWeek.TUESDAY.name(),
+        DayOfWeek.WEDNESDAY.name(),
+        DayOfWeek.THURSDAY.name(),
+        DayOfWeek.FRIDAY.name(),
+        DayOfWeek.SATURDAY.name()
+    };
 
     int getMaxDayOf(int year, int month) {
         if (isLeapYear(year)) {
@@ -14,17 +28,16 @@ public class Calender {
     }
 
     void printCalenderOf(int year, int month, int maxDays) {
+        DayOfWeek startDay = getStartDayOf(year, month);
+        int startDayValue = (startDay == DayOfWeek.SUNDAY) ? 1 : startDay.getValue() + 1;
+
+        // 달력 출력 부분
         System.out.printf("      <<%4d년%d월>>\n", year, month);
-        System.out.println("일  월  화  수  목  금  토");
-        System.out.println("-------------------------");
-        for (int i = 1; i <= maxDays; i++) {
-            System.out.printf("%-3d ", i);
-            if (i % 7 == 0) {
-                System.out.println();
-            }
-        }
-        System.out.println();
-        System.out.println("-------------------------\n");
+        System.out.println(getShortenedWeekdayNames());
+        System.out.println("--------------------------");
+        printInitialSpaces(startDayValue);
+        printDays(startDayValue, maxDays);
+        System.out.println("--------------------------\n");
     }
 
     private boolean isLeapYear(int year) {
@@ -34,6 +47,39 @@ public class Calender {
             return true;
         } else {
             return year % 400 == 0;
+        }
+    }
+
+    public DayOfWeek getStartDayOf(int year, int month) {
+        LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
+        return firstDayOfMonth.getDayOfWeek();
+    }
+
+    private String getShortenedWeekdayNames() {
+        return Arrays.stream(DAYS_OF_WEEK)
+            .map(week -> week.substring(0, 2))
+            .collect(Collectors.joining("  "));
+    }
+
+    private void printInitialSpaces(int startDayValue) {
+        for (int i = 1; i < startDayValue; i++) {
+            System.out.print("    ");
+        }
+    }
+
+    private void printDays(int startDayValue, int maxDays) {
+        int currentDay = startDayValue;
+        for (int i = 1; i <= maxDays; i++) {
+            System.out.printf("%-3d ", i);
+            if (currentDay % 7 == 0) {
+                System.out.println();
+            }
+            currentDay++;
+            if (currentDay > 7) currentDay = 1;
+        }
+
+        if ((currentDay - 1) % 7 != 0) {
+            System.out.println();
         }
     }
 }
