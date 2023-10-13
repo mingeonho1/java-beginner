05c3b5c7d2489bcf09fb1f041fa09adaa764e552
HOYA <als77770@naver.com>
2023-10-13 10:02:06 +0900

    가상의 달력 찍기

--- src/main/java/calender/Calender.java
+++ src/main/java/calender/Calender.java
@@ -11,42 +11,47 @@ public class Calender {
         Calender calender = new Calender();
         Scanner sc = new Scanner(System.in);
 
-        int repeat = calender.readIntOf(sc, "반복 횟수를 입력하세요.");
-
-        for (int i = 0; i < repeat; i++) {
-            int month = calender.getValidMonth(sc);
+        while (true) {
+            int month = calender.promptForMonth(sc,
+                """
+                    월을 입력하세요 : 1 ~ 12
+                    나기기 : -1
+                    """);
+            if (month == -1) {
+                System.out.println("종료됨.");
+                break;
+            }
 
             int maxDays = calender.getMaxDayOf(month);
 
-            calender.printCalenderOf(maxDays);
-
-            System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays);
+            calender.printCalenderOf(2023, month, maxDays);
         }
         sc.close();
     }
 
-    private int getValidMonth(Scanner sc) {
-        int month = 0;
-        boolean isValidInput = false;
-
-        while (!isValidInput) {
-            month = readIntOf(sc, "월을 입력하세요.");
-
-            if (isValidMonth(month)) {
-                isValidInput = true;
-            } else {
-                System.out.println("올바른 월(1에서 12 사이)을 입력하세요.");
+    private int promptForMonth(Scanner sc, String message) {
+        while (true) {
+            try {
+                System.out.println(message);
+                int month = sc.nextInt();
+                if (month == -1 || isValidMonth(month)) {
+                    return month;
+                } else {
+                    System.out.println("월은 1부터 12까지의 숫자로 입력해주세요.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("올바른 숫자를 입력하세요.");
+                sc.nextLine();
             }
         }
-
-        return month;
     }
 
     private int getMaxDayOf(int month) {
         return MAX_DAYS[month - 1];
     }
 
-    private void printCalenderOf(int maxDays) {
+    private void printCalenderOf(int year, int month, int maxDays) {
+        System.out.printf("     <<%4d년%d월>>\n", year, month);
         System.out.println("일  월  화  수  목  금  토");
         System.out.println("-------------------------");
         for (int i = 1; i <= maxDays; i++) {
@@ -56,35 +61,11 @@ public class Calender {
             }
         }
         System.out.println();
-        System.out.println("-------------------------");
+        System.out.println("-------------------------\n");
     }
 
     private boolean isValidMonth(int month) {
         return month >= 1 && month <= 12;
-    }
-
-    private int readIntOf(Scanner sc, String message) {
-        int i = 0;
-        try {
-            System.out.println(message);
-            i = sc.nextInt();
-        } catch (InputMismatchException e) {
-            System.out.println("올바른 숫자를 입력하세요.");
-            sc.nextLine();
-        }
-        return i;
-    }
-
-    private void printSampleCalender() {
-        System.out.println("일  월  화  수  목  금  토");
-        System.out.println("-------------------------");
-        System.out.println("8  9  10 11 12 13 14");
-        System.out.println("1  2  3  4  5  6  7");
-        System.out.println("15 16 17 18 19 20 21");
-        System.out.println("15 16 17 18 19 20 21");
-        System.out.println("22 23 24 25 26 27 28");
-        System.out.println("29 30");
-        System.out.println("-------------------------");
     }
 }
 
